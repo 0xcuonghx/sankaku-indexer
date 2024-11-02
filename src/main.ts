ae8e387c758fc890a3f7ee561797eb5974c2bb11
cuonghx <cuonghx@gmail.com>
2024-11-02 16:56:41 +0700

    Add Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Sankaku Payment API')
+    .setDescription('Sankaku Payment API for Sankaku Payment System')
+    .setVersion('0.9.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get<number>('PORT') || 3000);
 }
